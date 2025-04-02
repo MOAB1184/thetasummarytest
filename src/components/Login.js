8ca864e2c607fd26db3a604673a3bb176dd0cca2
MOAB1184 <nikitadecorah@gmail.com>
2025-04-02 16:34:24 -0400

    Ensure Login form uses text input type for email field

--- src/components/Login.js
+++ src/components/Login.js
@@ -123,7 +123,7 @@ function Login() {
         <div className="form-group">
           <label htmlFor="email">Email</label>
           <input
-            type="email"
+            type="text"
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
